1f8de3c6daef344ec61c833fc80a9798fd522150
Matty <hello@matty.co.za>
2013-03-08 10:16:19 +0200

    Removes trailing spaces from main plugin file.

--- woosidebars.php
+++ woosidebars.php
@@ -9,14 +9,14 @@
  * Stable tag: 1.2.2
  * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
- 
+
  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
  if ( ! class_exists( 'Woo_Conditions' ) ) {
  	require_once( 'classes/class-woo-conditions.php' );
  }
  require_once( 'classes/class-woo-sidebars.php' );
- 
+
  // Third-party integrations.
  if ( class_exists( 'Woocommerce' ) ) require_once( 'integrations/integration-woocommerce.php' );
 
